2f580ea217ff3516e9d1cd6334c3e8afcc573f1e
kevinus-59 <k.serrour@free.fr>
2019-05-10 14:13:15 +0200

    ajout exercice fusee

---
+++ src/calculEtDecouverteDesVariables/decollageDeFusee.java
@@ -0,0 +1,13 @@
+package calculEtDecouverteDesVariables;
+
+public class decollageDeFusee {
+        public static void main(String[] args) {
+            int decompte = 1;
+            for (int loop = 100; loop <=1; loop = loop - 1) {
+                System.out.println(decompte);
+                decompte = decompte -1;
+            }
+            System.out.println("Décollage ! ");
+        }
+
+}
