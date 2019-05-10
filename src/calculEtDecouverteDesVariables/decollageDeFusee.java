56ff63fea4bcdd2e8b5ef52ec3c3ff960339dde7
kevinus-59 <k.serrour@free.fr>
2019-05-10 15:10:19 +0200

    correction de l'exercice fusee

--- src/calculEtDecouverteDesVariables/decollageDeFusee.java
+++ src/calculEtDecouverteDesVariables/decollageDeFusee.java
@@ -2,8 +2,8 @@ package calculEtDecouverteDesVariables;
 
 public class decollageDeFusee {
         public static void main(String[] args) {
-            int decompte = 1;
-            for (int loop = 100; loop <=1; loop = loop - 1) {
+            int decompte = 100;
+            for (int loop = 1; loop <=101; loop = loop + 1) {
                 System.out.println(decompte);
                 decompte = decompte -1;
             }
