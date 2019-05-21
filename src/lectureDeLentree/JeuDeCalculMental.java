ea960753ad86e2b549751cfb3517c46747f87f85
kevinus-59 <k.serrour@free.fr>
2019-05-21 17:33:41 +0200

    chapitre lectureDeLentree ajout exercice 6

--- src/lectureDeLentree/JeuDeCalculMental.java
+++ src/lectureDeLentree/JeuDeCalculMental.java
@@ -1,11 +1,41 @@
+// ma solution qui fonctionne sur FranceIOI
+
 package lectureDeLentree;
 
 import java.util.Scanner;
 
-public class JeuDeCalculMental {
-    static Scanner entree = new Scanner(System.in);
 
-    public static void main (String[] args) {
-        int nbNobres = entree.nextInt();
+public class JeuDeCalculMental {
+
+    static Scanner Clavier = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int nbNombre = Clavier.nextInt();
+        int resultat = 66;
+        int facteur = 1;
+        for (int loop = 1; loop <= nbNombre; loop = loop + 1) {
+            resultat = resultat * facteur;
+            System.out.println(resultat);
+            facteur = facteur + 1;
+        }
     }
 }
+
+
+// correction FEANCE IOI
+/*
+package lectureDeLentree;
+import java.util.Scanner;
+class Main {
+    static Scanner entree = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int nbNombres = entree.nextInt();
+        int resultat = 66;
+        for (int facteur = 1; facteur <= nbNombres; facteur = facteur + 1) {
+            resultat = resultat * facteur;
+            System.out.println(resultat);
+        }
+    }
+}
+*/
